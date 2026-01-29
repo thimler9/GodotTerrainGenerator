2ca91ecca069429417f828c7c10ef68e17cc6347
Tom Himler <thimler9@gmail.com>
2026-01-29 09:57:22 -0500

    Small changes

--- src/TerrainGeneration.Application.TerrainGenerator/TerrainChunk.cs
+++ src/TerrainGeneration.Application.TerrainGenerator/TerrainChunk.cs
@@ -18,6 +18,7 @@ namespace TerrainGeneration.Application.TerrainGenerator
         /// <param name="descriptor"></param>
         public TerrainChunk(TransvoxelTerrainGenerator transvoxelTerrainGenerator, TerrainChunkDescriptor descriptor)
         {
+            // Setup transvoxel terrain generator for this chunk
             transvoxelTerrainGenerator.SetSDFShaderParameters(new SDFGenerator.SDFShaderParameters(descriptor.ChunkOffset, descriptor.ChunkSize, descriptor.Lod));
             TerrainMesh = transvoxelTerrainGenerator.GetTerrainMesh();
 
